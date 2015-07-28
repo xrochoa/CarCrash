1d999c1ccc6db0f5e432d5b275ca1c9e87a20f21
Xavier Reyes Ochoa <xrochoa@users.noreply.github.com>
2015-07-27 22:17:33 -0700

    added truck, collisions and score

--- boot.js
+++ boot.js
@@ -5,6 +5,8 @@ var mainState = {
         this.load.image('floor1', 'assets/floor1.png');
         this.load.image('car', 'assets/car.png');
         this.load.image('enemy', 'assets/enemy.png');
+        this.load.image('truck', 'assets/truck.png');
+
     },
 
     create: function() {
@@ -26,6 +28,17 @@ var mainState = {
         this.road = this.add.tileSprite(0, 34 * pixelScale, 30, 13, 'road');
         this.scaleSprite(this.road);
 
+        this.truck1 = this.add.sprite(-9 * pixelScale, lane1, 'truck');
+        this.scaleSprite(this.truck1);
+        game.physics.arcade.enable(this.truck1);
+
+
+        this.truck2 = this.add.sprite(-9 * pixelScale, lane2, 'truck');
+        this.scaleSprite(this.truck2);
+        game.physics.arcade.enable(this.truck2);
+
+
+
 
         this.car = this.add.sprite(5 * pixelScale, 36 * pixelScale, 'car');
         this.scaleSprite(this.car);
@@ -37,9 +50,16 @@ var mainState = {
 
         this.enemy1 = this.enemies.create(30 * pixelScale, lane1, 'enemy');
         this.scaleSprite(this.enemy1);
+        game.physics.arcade.enable(this.enemy1);
+
 
         this.enemy2 = this.enemies.create(60 * pixelScale, lane2, 'enemy');
         this.scaleSprite(this.enemy2);
+        game.physics.arcade.enable(this.enemy2);
+
+
+        this.label = game.add.text(100, 100, "Score");
+        this.score = 0;
 
 
 
@@ -72,61 +92,91 @@ var mainState = {
             }, 300, Phaser.Easing.Sinusoidal.InOut, true, 0);
 
         }
-        if (this.enemy1.x < 0) {
+        if (this.enemy1.x <= -this.enemy1.width) {
+            this.enemy1.y = lane1 + (lane2 - lane1) * game.rnd.integerInRange(0, 1);
             this.enemy1.x = 60 * pixelScale;
+            this.score++;
         }
 
-        if (this.enemy2.x < 0) {
+        if (this.enemy2.x <= -this.enemy2.width) {
+            this.enemy2.y = lane1 + (lane2 - lane1) * game.rnd.integerInRange(0, 1);
+
             this.enemy2.x = 60 * pixelScale;
+            this.score++;
+
         }
 
+        this.label.text = this.score;
+
+
+        if (Phaser.Rectangle.intersects(this.car.getBounds(), this.truck1.getBounds())) {
+            this.truck1.body.velocity.x = 300;
+            this.car.kill();
+
+
+        }
+        if (Phaser.Rectangle.intersects(this.car.getBounds(), this.truck2.getBounds())) {
+            this.truck2.body.velocity.x = 300;
+            this.car.kill();
+
+
+
+        }
+
+        if (Phaser.Rectangle.intersects(this.car.getBounds(), this.enemy1.getBounds()) || Phaser.Rectangle.intersects(this.car.getBounds(), this.enemy2.getBounds())) {
+                this.car.kill();
+
+
+
+            }
 
 
 
 
 
 
-    },
-
-    render: function() {
+        },
 
 
-    },
+        render: function() {
 
-    scaleSprite: function(sprite) {
-        sprite.scale.x = pixelScale;
-        sprite.scale.y = pixelScale;
-    },
 
-    gameStart: function() {
-        this.car.body.gravity.x = gravity;
-        this.enemy1.body.velocity.x = -300;
-        this.enemy2.body.velocity.x = -300;
+        },
 
-        gameInit = true;
+        scaleSprite: function(sprite) {
+            sprite.scale.x = pixelScale;
+            sprite.scale.y = pixelScale;
+        },
+
+        gameStart: function() {
+            this.car.body.gravity.x = gravity;
+            this.enemy1.body.velocity.x = -300;
+            this.enemy2.body.velocity.x = -300;
+
+            gameInit = true;
+
+        }
+    };
+
+    var mainMenu = {
+        preload: function() {
+            this.load.image('mainMenu', 'assets/lv1.png');
+
+
+        },
+        create: function() {
+            this.backgroundImage = this.add.tileSprite(0, 0, 30, 60 * pixelScale, 'mainMenu');
+            this.backgroundImage.inputEnabled = true;
+            this.backgroundImage.events.onInputDown.addOnce(this.startGame, this);
+
+
+
+        },
+        update: function() {
+
+        },
+        startGame: function() {
+            this.state.start('main');
+        }
 
     }
-};
-
-var mainMenu = {
-    preload: function() {
-        this.load.image('mainMenu', 'assets/lv1.png');
-
-
-    },
-    create: function() {
-        this.backgroundImage = this.add.tileSprite(0, 0, 30, 60 * pixelScale, 'mainMenu');
-        this.backgroundImage.inputEnabled = true;
-        this.backgroundImage.events.onInputDown.addOnce(this.startGame, this);
-
-
-
-    },
-    update: function() {
-
-    },
-    startGame: function() {
-        this.state.start('main');
-    }
-
-}
